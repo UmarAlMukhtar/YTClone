883e1a8faec0bdf0d632759c909122e72d462182
Umar Al Mukhtar <umar1868807@gmail.com>
2025-05-25 15:28:18 +0530

    initial commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState, useEffect, use } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import reactPlayer from 'react-player';
+import { Box, Typography, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from './';
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [channelDetail, setChannelDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => {
+        setVideoDetail(data.items[0]);
+      });
+  }, [id]);
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight={'95vh'}>
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <reactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
